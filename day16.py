95b57e386c084f2d958633e18e1fb379addb7b38
sgtpanic <sgtpanic@example.com>
2017-12-17 08:52:13 -0500

    start day 16

--- day16.py
+++ day16.py
@@ -1,22 +1,51 @@
-programs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
-places = dict()
-for i in range(len(programs)):
-    places[programs[i]] = i
+def swap(start, end, programs):
+    temp = programs[start]
+    programs[start] = programs[end]
+    programs[end] = temp
 
-with open("resources/testing", "rt") as f:
+
+def print_programs(programs):
+    for each_program in programs:
+        print(each_program, end="")
+    print()
+
+
+with open("resources/day16", "rt") as f:
     for line in f:
+        programs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
+        original = list(programs)
         split = line.split(',')
-        for each in split:
-            action = each[0]
-            if action == 's':
-                number = int(each[1:])
-                temp = list()
-                for i in range(len(programs) - number, len(programs)):
-                    temp.append(programs[i])
-                for i in range(0, len(programs) - number):
-                    temp.append(programs[i])
-            elif action == 'x':
-                pass
+        repeats = 0
+        rearranged = list()
+        for each_round in range(1000000000 % 24):
+            for each in split:
+                action = each[0]
+                if action == 's':
+                    number = int(each[1:])
+                    temp = list()
+                    for last in range(len(programs) - number, len(programs)):
+                        temp.append(programs[last])
+                    for first in range(0, len(programs) - number):
+                        temp.append(programs[first])
+                    programs = temp
+                elif action == 'x':
+                    programs_split = each.split('/')
+                    start = int(programs_split[0][1:])
+                    end = int(programs_split[1])
+                    swap(start, end, programs)
+                elif action == 'p':
+                    programs_split = each.split('/')
+                    start = programs_split[0][1:]
+                    end = programs_split[1]
+                    start_index = -1
+                    end_index = -1
+                    for index in range(len(programs)):
+                        if start == programs[index]:
+                            start_index = index
+                        elif end == programs[index]:
+                            end_index = index
+                    swap(start_index, end_index, programs)
+        print_programs(programs)
 
-            elif action == 'p':
-                pass
+
+
