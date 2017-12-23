e5b781671c5555822103aeb42e4f8a17ac9c2308
sgtpanic <sgtpanic@example.com>
2017-12-23 13:46:57 -0500

    finish day 18

--- day18.py
+++ day18.py
@@ -1,26 +1,31 @@
 import queue
 import threading
+import copy
 import time
 
 
-def read_instructions(each_registers, opposing_queue, my_queue):
+def read_instructions(id, each_registers, opposing_queue, my_queue):
     current_instruction = 0
     sent = 0
+
     while True:
         action = instructions[current_instruction][0]
         register = instructions[current_instruction][1]
         value = 0
+
         if len(instructions[current_instruction]) == 3:
             value = instructions[current_instruction][2]
-            if str(value).startswith('-') or value.isdigit():
-                value = int(value)
-            else:
-                value = each_registers[value]
+        else:
+            value = instructions[current_instruction][1]
+
+        if value.startswith('-') or value.isdigit():
+            value = int(value)
+        else:
+            value = each_registers[value]
 
         if action == 'snd':
             opposing_queue.put(value)
             sent += 1
-            print(sent)
         elif action == 'set':
             each_registers[register] = value
         elif action == 'add':
@@ -31,10 +36,14 @@ def read_instructions(each_registers, opposing_queue, my_queue):
             each_registers[register] %= value
         elif action == 'rcv':
             while my_queue.empty():
+                if opposing_queue.empty():
+                    if id == 1:
+                        print(sent)
+                    return
                 continue
-            each_registers[register] = my_queue.pop()
+            each_registers[register] = my_queue.get()
         elif action == 'jgz':
-            if each_registers[register] > 0:
+            if (register.isdigit() and int(register) > 0) or each_registers[register] > 0:
                 current_instruction += value
             else:
                 current_instruction += 1
@@ -52,23 +61,20 @@ with open("resources/day18", "rt") as f:
     for line in f:
         split = line.split()
         instructions.append(split)
-        registers[split[1]] = 0
+        if split[1].isalpha():
+            registers[split[1]] = 0
 
     queue_0 = queue.Queue()
     queue_1 = queue.Queue()
-    registers_0 = dict(registers)
+    registers_0 = copy.deepcopy(registers)
     registers_0['p'] = 0
-    registers_1 = dict(registers)
+    registers_1 = copy.deepcopy(registers)
     registers_1['p'] = 1
 
-    thread_0 = threading.Thread(target=read_instructions, args=(registers_0, queue_1, queue_0))
-    thread_0.daemon = True
+    thread_0 = threading.Thread(target=read_instructions, args=(0, registers_0, queue_1, queue_0))
     thread_0.start()
-    time.sleep(5)
-    thread_1 = threading.Thread(target=read_instructions, args=(registers_1, queue_0, queue_1))
-    thread_1.daemon = True
+    thread_1 = threading.Thread(target=read_instructions, args=(1, registers_1, queue_0, queue_1))
     thread_1.start()
 
-    while queue_0.empty() and queue_1.empty():
+    while thread_1.is_alive() and thread_0.is_alive():
         continue
-
